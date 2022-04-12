46f9bb8bffa4b384363e08b8fff1aeeefc70eb31
P William Hozier <philron.hozier@modaoperandi.com>
2022-04-12 09:23:21 -0400

    Further

--- src/main/scala/com/rockthejvm/part2effects/Effects.scala
+++ src/main/scala/com/rockthejvm/part2effects/Effects.scala
@@ -3,6 +3,8 @@ package com.rockthejvm.part2effects
 import scala.concurrent.Future
 
 /** @note
+  *   FP: implemented `descriptions` of computation
+  * @example
   *   Concretely: an `effect` is a `data type` that embodies the concept of a) a
   *   side effect or b) any sort of computation that we might need in our code.
   *   Abstractly: the concept of a purely functional program may be defined as a
@@ -11,5 +13,20 @@ import scala.concurrent.Future
   *   expr)
   */
 object Effects {
+
+  /** a) custom IO `describes` the value that will be calculated inside.
+    *
+    * b) the data structure `creation` is seperate from
+    *
+    * c) the performance of the computation.
+    */
+  case class IO[A](unsafeRun: () => A) {
+    def map[B](f: A => B): IO[B] = {
+      IO(() => f(unsafeRun()))
+    }
+    def flatMap[B](f: A => IO[B]): IO[B] = {
+      IO(() => f(unsafeRun()).unsafeRun())
+    }
+  }
   def main(args: Array[String]): Unit = {}
 }
