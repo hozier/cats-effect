f13b9991f1f799b6b322474bcee066c75c14796e
P. William Hozier <hozier@users.noreply.github.com>
2022-04-11 23:54:23 -0400

    Introduction: Essentials and Contextual Abstractions (#1)
    * Initial env setup
    
    * Futures review
    
    * Understanding using/given combinations
    
    * Implement  functionality to various types
    
    * start with blank object implementation
    
    * Cats type clases reviewed
    
    Co-authored-by: P William Hozier <philron.hozier@modaoperandi.com>

--- src/main/scala/com/rockthejvm/part1recap/CatsTypeClasses.scala
+++ src/main/scala/com/rockthejvm/part1recap/CatsTypeClasses.scala
@@ -1,104 +1,64 @@
 package com.rockthejvm.part1recap
 
+/** @note
+  *   Covers: Fundamental Cats type classes needed to develop with cats-effect.
+  *   Cats is a functional programming library based on type classes. Cats has
+  *   various type classes which include:
+  *
+  * a) functor b) applicative c) flatMap d) monad e) apply f)
+  * applicativeError/mondaError g) traverse
+  */
 object CatsTypeClasses {
 
-  /*
-    - applicative
-    - functor
-    - flatMap
-    - monad
-    - applicativeError/monadError
-    - traverse
-   */
+  /** a) functor
+    * @note
+    *   describes a data type that is mappable, eg collections/containers. we
+    *   can view the below higher kinded as an untyped `container` described by
+    *   `F`. functors are used to define very generalisable apis.
+    */
 
-  // functor - "mappable" data structures
-  trait MyFunctor[F[_]] {
+  trait FUNCTOR[F[_]] {
     def map[A, B](initialValue: F[A])(f: A => B): F[B]
   }
 
-  import cats.Functor
-  import cats.instances.list.*
-  val listFunctor = Functor[List]
-
-  // generalizable "mapping" APIs
-  def increment[F[_]](container: F[Int])(using functor: Functor[F]): F[Int] =
-    functor.map(container)(_ + 1)
-
-  import cats.syntax.functor.*
-  def increment_v2[F[_] : Functor](container: F[Int]): F[Int] =
-    container.map(_ + 1)
-
-  // applicative - the ability to "wrap" types
-  trait MyApplicative[F[_]] extends MyFunctor[F] {
+  /** b) applicative
+    * @note
+    *   describes the ability to `wrap` types. the main api is `pure`. pure can
+    *   be thought of as a constructor of a type arg `F`, eg List, Option, etc
+    */
+  trait APPLICATIVE[F[_]] extends FUNCTOR[F] {
     def pure[A](value: A): F[A]
   }
 
-  import cats.Applicative
-  val applicativeList = Applicative[List]
-  val aSimpleList: List[Int] = applicativeList.pure(43)
-  import cats.syntax.applicative.* // import the pure extension method
-  val aSimpleList_v2: List[Int] = 43.pure[List]
-
-  // FlatMap - ability to chain multiple wrapper computations
-  trait MyFlatMap[F[_]] extends MyFunctor[F] {
+  /** c) flatMap
+    * @note
+    *   describes the ability of `chaining` wrapper multiple computations.
+    */
+  trait FLATMAP[F[_]] extends FUNCTOR[F] {
     def flatMap[A, B](container: F[A])(f: A => F[B]): F[B]
   }
 
-  import cats.FlatMap
-  val flatMapList = FlatMap[List]
-  import cats.syntax.flatMap.* // flatMap extension method
-  def crossProduct[F[_] : FlatMap, A, B](fa: F[A], fb: F[B]): F[(A, B)] =
-    fa.flatMap(a => fb.map(b => (a, b)))
-
-  // Monad - applicative + flatMap
-  trait MyMonad[F[_]] extends MyApplicative[F] with MyFlatMap[F] {
-    override def map[A, B](initialValue: F[A])(f: A => B): F[B] =
-      flatMap(initialValue)(a => pure(f(a)))
+  /** d) monad
+    * @note
+    *   describes the combination of applicative and flatMap. monad blends the
+    *   concepts together to deliver the most unifying foundation of functional
+    *   programming
+    */
+  trait MONAD[F[_]] extends APPLICATIVE[F] with FLATMAP[F] {
+    // this is implemented in terms of `pure` and `flatMap`
+    override def map[A, B](initialValue: F[A])(f: A => B): F[B] = {
+      flatMap(initialValue)(a =>
+        /** f(a) === type B, !F[B], so we must wrap it with `pure` === F[B] */
+        pure(f(a))
+      )
+    }
   }
 
-  import cats.Monad
-  val monadList = Monad[List]
-  def crossProduct_v2[F[_] : Monad, A, B](fa: F[A], fb: F[B]): F[(A, B)] =
-    for {
-      a <- fa
-      b <- fb
-    } yield (a, b)
-
-  /*
-         Functor -> FlatMap -->
-                 \             \
-                    Applicative -> Monad
-   */
-
-  // error-like type classes
-  trait MyApplicativeError[F[_], E] extends MyApplicative[F] {
-    def raiseError[A](e: E): F[A]
-  }
-
-  import cats.ApplicativeError
-  type ErrorOr[A] = Either[String, A]
-  val appErrorEither = ApplicativeError[ErrorOr, String]
-  val desirableValue: ErrorOr[Int] = appErrorEither.pure(42)
-  val failedValue: ErrorOr[Int] = appErrorEither.raiseError("Something failed")
-  import cats.syntax.applicativeError.* // raiseError extension method
-  val failedValue_v2: ErrorOr[Int] = "Something failed".raiseError[ErrorOr, Int]
-
-  trait MyMonadError[F[_], E] extends MyApplicativeError[F, E] with Monad[F]
-  import cats.MonadError
-  val monadErrorEither = MonadError[ErrorOr, String]
-
-  // traverse
-  trait MyTraverse[F[_]] extends MyFunctor[F] {
+  /** g) traverse
+    */
+  trait TRAVERSE[F[_]] extends FUNCTOR[F] {
     def traverse[G[_], A, B](container: F[A])(f: A => G[B]): G[F[B]]
   }
-
-  // turn nested wrappers inside out
-  val listOfOptions: List[Option[Int]] = List(Some(1), Some(2), Some(43))
-  import cats.Traverse
-  val listTraverse = Traverse[List]
-  val optionList: Option[List[Int]] = listTraverse.traverse(List(1,2,3))(x => Option(x))
-  import cats.syntax.traverse.*
-  val optionList_v2: Option[List[Int]] = List(1,2,3).traverse(x => Option(x))
 
   /*
     Big(ger) type class hierarchy in Cats:
@@ -113,7 +73,5 @@ object CatsTypeClasses {
          |
          ----> Traverse
    */
-  def main(args: Array[String]): Unit = {
-
-  }
+  def main(args: Array[String]): Unit = {}
 }
--- src/main/scala/com/rockthejvm/part1recap/ContextualAbstractionsScala3.scala
+++ src/main/scala/com/rockthejvm/part1recap/ContextualAbstractionsScala3.scala
@@ -1,107 +1,81 @@
 package com.rockthejvm.part1recap
 
+/** @note
+  *   Covers: 1 - given instances, 2 - extension methods, 3 - combining
+  *   given/using to implement type classes
+  */
 object ContextualAbstractionsScala3 {
-
-  // given/using combo
-  def increment(x: Int)(using amount: Int): Int = x + amount
-  given defaultAmount: Int = 10
-  val twelve = increment(2) // (10) automatically by the compiler
-
-  def multiply(x: Int)(using factor: Int): Int = x * factor
-  val aHundred = multiply(10) // defaultAmount is passed automatically
-
-  // more complex use case
-  trait Combiner[A] {
-    def combine(x: A, y: A): A
-    def empty: A
-  }
-
-  def combineAll[A](values: List[A])(using combiner: Combiner[A]): A =
-    values.foldLeft(combiner.empty)(combiner.combine)
-
-  given intCombiner: Combiner[Int] with {
-    override def combine(x: Int, y: Int) = x + y
-    override def empty = 0
-  }
-
-  val numbers = (1 to 10).toList
-  val sum10 = combineAll(numbers) // intCombiner passed automatically
-
-  // synthesize given instances
-  given optionCombiner[T](using combiner: Combiner[T]): Combiner[Option[T]] with {
-    override def empty = Some(combiner.empty)
-    override def combine(x: Option[T], y: Option[T]): Option[T] = for {
-      vx <- x
-      vy <- y
-    } yield combiner.combine(vx, vy)
-  }
-
-  val sumOptions: Option[Int] = combineAll(List(Some(1), None, Some(2)))
-
-  // extension methods
-  case class Person(name: String) {
-    def greet(): String = s"Hi, my name is $name"
-  }
-
-  extension (name: String)
-    def greet(): String = Person(name).greet()
-
-  val alicesGreeting = "Alice".greet()
-
-  // generic extension
-  extension [T](list: List[T])
-    def reduceAll(using combiner: Combiner[T]): T =
-      list.foldLeft(combiner.empty)(combiner.combine)
-
-  val sum10_v2 = numbers.reduceAll
-
-
   def main(args: Array[String]): Unit = {
 
-  }
-}
+    /** @note
+      *   1 - given instances are equiv to implicit args/value from scala 2
+      */
+    def increment: (Int => (Int => Int)) = x => (amount) => x + amount
+    val twelve = increment(10)(2)
 
-object TypeClassesScala3 {
-  case class Person(name: String, age: Int)
+    /** given/using pairs allow us to anchor an argument value to a function
+      * when `given` is applied, the value declared is automatically passed by
+      * the compiler to the `using` variable
+      */
+    given defaultAmount: Int = 10
+    def incrementWithUsing(x: Int)(using amount: Int): Int = x + amount
+    val thirteen = incrementWithUsing(3)
+    println(s"thirteen=$thirteen")
 
-  // type classes
+    /** @example start ****************************************************** */
+    /** Combiner overview: Display by the example which follows how to `attach`
+      * functionality to certain types. Follow up with displaying composability
+      * of attachments created.
+      */
 
-  // part 1 - Type class definition
-  trait JSONSerializer[T] {
-    def toJson(value: T): String
-  }
+    /** the standalone version of a monoid implemented. below is the standard
+      * way of processing or reducing a list with some initial seed and some
+      * combination function
+      */
+    trait Combiner[A] {
+      def combine(x: A, y: A): A
+      def empty: A
+    }
 
-  // part 2 - type class instances
-  given stringSerializer: JSONSerializer[String] with {
-    override def toJson(value: String) = "\"" + value + "\""
-  }
+    def combineAll[A](values: List[A])(using combiner: Combiner[A]): A = {
+      values.foldLeft(combiner.empty)(combiner.combine)
+    }
 
-  given intSerializer: JSONSerializer[Int] with {
-    override def toJson(value: Int) = value.toString
-  }
+    /** combineAll is available for Lists of any type, for which we have a
+      * `given` `Combiner` in scope, eg, `given intCombiner: Combiner[Int] with
+      * {...}`
+      */
+    given intCombiner: Combiner[Int] with {
+      override def combine(x: Int, y: Int) = x + y
+      override def empty = 0
+    }
 
-  given personSerializer: JSONSerializer[Person] with {
-    override def toJson(person: Person) =
-      s"""
-         |{"name": "${person.name}", "age": ${person.age}}
-         |""".stripMargin.trim
-  }
+    /** @note
+      *   execute on `combineAll` List[Int] example
+      * @description
+      *   The List[Int] sent in below is passed to the defined instance of
+      *   `Combiner` (that the compiler requires). This instance known as
+      *   `intCombiner` is passed automatically and leveraged internally by
+      *   combineAll.
+      */
+    println(combineAll((1 to 10).toList)) /* so. so. cool! */
 
-  // part 3 - user-facing API
-  def convert2Json[T](value: T)(using serializer: JSONSerializer[T]): String =
-    serializer.toJson(value)
+    /** @example abstract through composition ***************************** */
+    // synthesize given instances
+    given optionCombiner[T](using combiner: Combiner[T]): Combiner[Option[T]]
+      with {
+      override def empty = Some(combiner.empty)
+      override def combine(x: Option[T], y: Option[T]): Option[T] = for {
+        /** unpacks options and then passes them to combine */
+        vx <- x
+        vy <- y
+      } yield combiner.combine(vx, vy)
+    }
 
-  def convertList2Json[T](list: List[T])(using serializer: JSONSerializer[T]): String =
-    list.map(value => serializer.toJson(value)).mkString("[", ",", "]")
+    println(
+      combineAll(List(Option(1), Some(2), Some(3)))
+    ) /* even. cooler! */
+    /** @example end ****************************************************** */
 
-  // part 4 - extension methods just for the types we support
-  extension [T](value: T)
-    def toJson(using serializer: JSONSerializer[T]): String =
-      serializer.toJson(value)
-
-  def main(args: Array[String]): Unit = {
-    println(convertList2Json(List(Person("Alice", 23), Person("Bob", 46))))
-    val bob = Person("Bob", 46)
-    println(bob.toJson)
   }
 }
