226521ec90f26f3c2596be8dc53d95538b9f0b22
P William Hozier <philron.hozier@modaoperandi.com>
2022-04-13 12:59:39 -0400

    Initial commit: IOTraversal.scala

---
+++ src/main/scala/com/rockthejvm/part2effects/IOTraversal.scala
@@ -0,0 +1,12 @@
+package com.rockthejvm.part2effects
+
+import cats.Parallel
+import cats.effect.{IO, IOApp}
+
+/** @note
+  *   Covers: cats.effect.IO - traverse concept.
+  */
+object IOTraversal extends IOApp.Simple {
+
+  override def run: IO[Unit] = IO {}
+}
