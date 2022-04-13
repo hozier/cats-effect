243e7d8b412e7958030f0e913fcb6f7822dd3edd
P William Hozier <philron.hozier@modaoperandi.com>
2022-04-13 07:51:11 -0400

    briefly review IOErrorHandling with Either

--- src/main/scala/com/rockthejvm/part2effects/IOErrorHandling.scala
+++ src/main/scala/com/rockthejvm/part2effects/IOErrorHandling.scala
@@ -7,8 +7,14 @@ import scala.util.{Failure, Success, Try}
   *   Covers: cats.effect.IO - creating `failed` computations suspended in IO
   */
 object IOErrorHandling {
+  def effectAsEither: IO[Int] => IO[Either[Throwable, Int]] = n => n.attempt
   def main(args: Array[String]): Unit = {
     import cats.effect.unsafe.implicits.global
-
+    effectAsEither(IO(4)).flatMap(either =>
+      either match {
+        case Left(e)  => IO { println(s"rejected=$e") }
+        case Right(v) => IO { println(s"resolved=$v") }
+      }
+    )
   }
 }
